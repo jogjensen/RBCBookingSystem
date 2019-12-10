a700f4f7112b92b7996b900474bfd6c7985fca85
Niko-easj <58074551+Niko-easj@users.noreply.github.com>
2019-12-10 10:03:12 +0100

    Indsat RBCBooking class

---
+++ Datamatiker_1semester/Classes/RBCBooking.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.UserDataTasks;
+
+namespace Datamatiker_1semester.Classes
+{
+    public class RBCBooking
+    {
+        // _dato = "Dato - 5/12/2019";
+        // _tid = "10:45";
+
+        private string _tid;
+        private string _dato;
+
+        public string Tid
+        {
+            get => _tid;
+            set { _tid = value; }
+        }
+
+        public string Dato
+        {
+            get => _dato;
+            set { _dato = value; }
+        }
+
+        public RBCBooking(string Tid, string Dato)
+        {
+           _tid = Tid;
+           _dato = Dato;
+        }
+    }
+}
